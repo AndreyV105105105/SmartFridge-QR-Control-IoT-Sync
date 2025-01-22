5a79f367f5566bd6c6f9d0a8072b181f01be8f7d
Andrey <volvachandrey108@gmail.com>
2025-01-22 18:14:07 +0300

    prig prig

--- database.py
+++ database.py
@@ -134,27 +134,27 @@ class DatabaseManager:
             "added_at": row[9]
         }
 
-    def search_products(self, search_term=None, search_type=None):
-        """Поиск продуктов по названию и/или типу."""
-        if not self.conn or not self.cursor:
-            raise Exception("Нет подключения к БД. Сначала нужно вызвать connect()")
-        query = "SELECT * FROM products WHERE 1=1"
-        params = []
-        if search_term:
-            query += " AND product_name LIKE ?"
-            params.append(f"%{search_term}%")
-        if search_type:
-            query += " AND product_type LIKE ?"
-            params.append(f"%{search_type}%")
-
-        self.cursor.execute(query, params)
-        rows = self.cursor.fetchall()
-        products = []
-        for row in rows:
-            product = self._row_to_dict(row)
-            products.append(product)
-
-        return products
+    # def search_products(self, search_term=None, search_type=None):
+    #     """Поиск продуктов по названию и/или типу."""
+    #     if not self.conn or not self.cursor:
+    #         raise Exception("Нет подключения к БД. Сначала нужно вызвать connect()")
+    #     query = "SELECT * FROM products WHERE 1=1"
+    #     params = []
+    #     if search_term:
+    #         query += " AND product_name LIKE ?"
+    #         params.append(f"%{search_term}%")
+    #     if search_type:
+    #         query += " AND product_type LIKE ?"
+    #         params.append(f"%{search_type}%")
+    #
+    #     self.cursor.execute(query, params)
+    #     rows = self.cursor.fetchall()
+    #     products = []
+    #     for row in rows:
+    #         product = self._row_to_dict(row)
+    #         products.append(product)
+    #
+    #     return products
 
     def add_to_shopping_list(self, product_name, quantity):
         """Добавляет продукт в список покупок."""
