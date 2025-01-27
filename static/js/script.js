25d0a35b025bdef4a014321d77eadb4c9ce8fe44
Andrey <volvachandrey108@gmail.com>
2025-01-27 14:58:21 +0300

    product info on qr page

--- static/js/script.js
+++ static/js/script.js
@@ -17,13 +17,27 @@ $(document).ready(function() {
                 processData: false,
                 success: function(response) {
                     if (response.success) {
-                        $('#result').html('QR Code Data: ' + response.qr_data);
+                        const ex = JSON.parse(response.qr_data)
+                        $('#product_name').html(ex['название']);
+                        $('#full_information').html(' Подробная информация: ');
+                        $('#product_type').html(' Тип: ' + ex['тип']);
+                        $('#manufacture_date').html(' Дата изготовления: ' + ex['дата_изготовления']);
+                        $('#expiry_date').html(' Дата истечения срока годности: ' + ex['годен_до']);
+                        $('#quantity_unit').html('масса/объем, ед. измерения: ' + ex['масса_объем'] + ', ' +  ex['ед_измерения']);
+                        $('#measurement_type').html('Тип измерения: ' + ex['тип_измерения']);
+                        $('#nutrition_info').html('Пищевая ценность: ');
+                        $('#calories').html('Калории: ' + ex['пищевая_ценность']['калории']);
+                        $('#proteins').html('Белки: ' + ex['пищевая_ценность']['белки']);
+                        $('#fats').html('Жиры: ' + ex['пищевая_ценность']['жиры']);
+                        $('#carbohydrates').html('Углеводы: ' + ex['пищевая_ценность']['углеводы']);
+
+
                     } else {
-                        $('#result').html(response.message);
+                        $('#product_name').html(response.message);
                     }
                 },
                 error: function(xhr, status, error) {
-                    $('#result').html('Error processing image. Please try again.');
+                    $('#product_name').html('Error processing image. Please try again.');
                 }
             });
         }
