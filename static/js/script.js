580cef6876302b163d59d6e6225f98e6046cb491
Andrey <volvachandrey108@gmail.com>
2025-01-27 22:50:08 +0300

    for new qr-codes

--- static/js/script.js
+++ static/js/script.js
@@ -18,18 +18,18 @@ $(document).ready(function() {
                 success: function(response) {
                     if (response.success) {
                         const ex = JSON.parse(response.qr_data)
-                        $('#product_name').html(ex['название']);
+                        $('#product_name').html(ex['product_name']);
                         $('#full_information').html(' Подробная информация: ');
-                        $('#product_type').html(' Тип: ' + ex['тип']);
-                        $('#manufacture_date').html(' Дата изготовления: ' + ex['дата_изготовления']);
-                        $('#expiry_date').html(' Дата истечения срока годности: ' + ex['годен_до']);
-                        $('#quantity_unit').html('масса/объем, ед. измерения: ' + ex['масса_объем'] + ', ' +  ex['ед_измерения']);
-                        $('#measurement_type').html('Тип измерения: ' + ex['тип_измерения']);
+                        $('#product_type').html(' Тип: ' + ex['product_type']);
+                        $('#manufacture_date').html(' Дата изготовления: ' + ex['manufacture_date']);
+                        $('#expiry_date').html(' Дата истечения срока годности: ' + ex['expiry_date']);
+                        $('#quantity_unit').html('масса/объем, ед. измерения: ' + ex['quantity'] + ', ' +  ex['unit']);
+                        $('#measurement_type').html('Тип измерения: ' + ex['measurement_type']);
                         $('#nutrition_info').html('Пищевая ценность: ');
-                        $('#calories').html('Калории: ' + ex['пищевая_ценность']['калории']);
-                        $('#proteins').html('Белки: ' + ex['пищевая_ценность']['белки']);
-                        $('#fats').html('Жиры: ' + ex['пищевая_ценность']['жиры']);
-                        $('#carbohydrates').html('Углеводы: ' + ex['пищевая_ценность']['углеводы']);
+                        $('#calories').html('Калории: ' + ex['nutrition_info']["калории"]);
+                        $('#proteins').html('Белки: ' + ex['nutrition_info']["белки"]);
+                        $('#fats').html('Жиры: ' + ex['nutrition_info']["жиры"]);
+                        $('#carbohydrates').html('Углеводы: ' + ex['nutrition_info']["углеводы"]);
 
 
                     } else {
